fac3d9127b12507e8a7a66e1a07f574bdf56ec81
Kristian Rosenvold <kristian@zenior.no>
2011-06-29 09:34:10 +0200

    Fixed reporter logging

--- src/main/java/org/openqa/selenium/diags/Stats.java
+++ src/main/java/org/openqa/selenium/diags/Stats.java
@@ -33,119 +33,117 @@ public class Stats {
 
     private final ConcurrentHashMap<String, StatEvent> eventMap = new ConcurrentHashMap<String, StatEvent>();
 
-    private final AtomicInteger activeBrowsers = new AtomicInteger(  );
+    private final AtomicInteger activeBrowsers = new AtomicInteger();
 
-    private final AtomicInteger saveFileNumber = new AtomicInteger( 0 );
+    private final AtomicInteger saveFileNumber = new AtomicInteger(0);
 
     private final String fileName;
 
 
-    public Stats( String fileName )
-    {
+    public Stats(String fileName) {
         this.fileName = fileName;
     }
 
-    private void doReport()
-    {
-        File file = new File( fileName + saveFileNumber.incrementAndGet() + ".txt");
-        Map<String, StatEvent> copy = new HashMap<String, StatEvent>( eventMap);
+    private void doReport() {
+        File file = getReportFile();
+        Map<String, StatEvent> copy = new HashMap<String, StatEvent>(eventMap);
         eventMap.clear();
-        try
-        {
-            FileOutputStream fos = new FileOutputStream( file );
-            PrintStream ps = new PrintStream(  fos );
-            doReport(  ps, copy );
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            PrintStream ps = new PrintStream(fos);
+            doReport(ps, copy);
             ps.close();
             fos.close();
-        }
-        catch ( FileNotFoundException e )
-        {
-            throw new RuntimeException(  e );
-        }
-        catch ( IOException e )
-        {
-            throw new RuntimeException(  e );
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private Runnable getReporter(){
+    private void doReport(PrintStream out, Map<String, StatEvent> itemMap) {
+
+        Set<String> items = new TreeSet<String>(itemMap.keySet());
+        for (String key : items) {
+            StatEvent statEvent = itemMap.get(key);
+            out.println(key + "," + statEvent);
+        }
+    }
+
+    private File getReportFile() {
+        File file;
+        do {
+            file = new File(fileName + saveFileNumber.incrementAndGet() + ".txt");
+        } while (file.exists());
+        return file;
+    }
+
+    private Runnable getReporter() {
         return new Runnable() {
             public void run() {
                 doReport();
             }
         };
     }
-    private void doReport(PrintStream out, Map<String, StatEvent> itemMap)
-    {
-
-        Set<String> items = new TreeSet<String>(itemMap.keySet());
-        for ( String key : items )
-        {
-            StatEvent statEvent = itemMap.get(  key );
-            out.println( key + "," + statEvent);
-        }
-    }
 
 
-    public StatEventInstance create(String methodName, Object[] args){
-        return getOrCreate( methodName + "#" +  getKey(args)).instantiate();
+
+    public StatEventInstance create(String methodName, Object[] args) {
+        return getOrCreate(methodName + "#" + getKey(args)).instantiate();
     }
 
     private String getKey(Object[] args) {
         StringBuilder result = new StringBuilder();
-        if (args == null){
+        if (args == null) {
             return "null";
         }
         for (Object arg : args) {
-            if (arg instanceof  By){
-                result.append( toKey( (By) arg ) );
-            } else if ( arg instanceof Object[]){
+            if (arg instanceof By) {
+                result.append(toKey((By) arg));
+            } else if (arg instanceof Object[]) {
                 Object[] argArray = (Object[]) arg;
-                if (argArray.length > 0){
+                if (argArray.length > 0) {
                     // Maybe need to do something about arrays...
                 }
             } else {
-               result.append(arg != null ? arg.toString() : "null");
+                result.append(arg != null ? arg.toString() : "null");
             }
         }
         return result.toString();
     }
 
-    private StatEvent getOrCreate(String key){
+    private StatEvent getOrCreate(String key) {
         StatEvent event = new StatEvent();
         final StatEvent existing = eventMap.putIfAbsent(key, event);
         return existing != null ? existing : event;
     }
 
-    private String toKey( By by){
+    private String toKey(By by) {
         final String s = by.toString();
-        return StringUtils.substringBefore( s, ":" );
+        return StringUtils.substringBefore(s, ":");
     }
 
-    public void quit()
-    {
+    public void quit() {
         final int i = activeBrowsers.decrementAndGet();
-        if (i == 0){
+        if (i == 0) {
             Runnable runnable = new Runnable() {
                 public void run() {
                     try {
-                        Thread.sleep( 5000);
+                        Thread.sleep(5000);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
-                    if (activeBrowsers.get() == 0){
+                    if (activeBrowsers.get() == 0) {
                         getReporter().run();
                     }
                 }
             };
             new Thread(runnable).start();
-            doReport();
         }
     }
 
 
-    public void addWebDriver()
-    {
+    public void addWebDriver() {
         activeBrowsers.incrementAndGet();
     }
 
